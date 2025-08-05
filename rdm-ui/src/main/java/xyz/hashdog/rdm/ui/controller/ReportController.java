d69d4722c83da6982d53e5484dbea0c3925414aa
1103617438@qq.com <1103617438@qq.com>
2025-08-05 14:10:02 +0800

    feat(ui): 优化报告页面样式和布局
    - 添加主题支持，动态设置图表背景色和字体样式
    - 调整图表间距和布局，提高可读性- 为 BarChart 和 LineChart 添加 fx:id，支持控制器中的引用和配置

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/ReportController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/ReportController.java
@@ -8,11 +8,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -21,8 +24,12 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Shape;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import xyz.hashdog.rdm.ui.sampler.theme.SamplerTheme;
+import xyz.hashdog.rdm.ui.sampler.theme.ThemeManager;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import static xyz.hashdog.rdm.ui.sampler.page.Page.FAKER;
@@ -30,9 +37,32 @@ import static xyz.hashdog.rdm.ui.sampler.page.Page.FAKER;
 public class ReportController extends BaseKeyController<ServerTabController> implements Initializable {
     public PieChart memory;
     public PieChart keys;
+    public HBox pies;
+    public BarChart bar;
+    public LineChart line;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        SamplerTheme theme = ThemeManager.getInstance().getTheme();
+        int fontSize = ThemeManager.getInstance().getFontSize();
+        String fontFamily = ThemeManager.getInstance().getFontFamily();
+        Map<String, String> colors = null;
+        try {
+            colors = theme.parseColors();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String c1 = colors.get("-color-bg-subtle");
+        pies.setStyle("-fx-background-color:"+c1);
+        bar.setStyle("-fx-background-color:"+c1);
+        // 设置图表绘图区域背景色
+        bar.lookup(".chart-plot-background").setStyle("-fx-background-color: transparent");
+
+// 设置坐标轴区域背景色
+        bar.lookup(".axis").setStyle("-fx-background-color: transparent");
+        line.setStyle("-fx-background-color:"+c1);
+
+
         final var rnd = FAKER.random();
 
         ObservableList<PieChart.Data> data = FXCollections.observableArrayList(
