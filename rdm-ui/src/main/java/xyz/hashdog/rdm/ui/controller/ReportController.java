563fa6d9f4649281f6ed86fdb70ac62a3f3e3927
1103617438@qq.com <1103617438@qq.com>
2025-08-05 11:52:34 +0800

    feat(ui): 添加饼图报告页面样式和交互效果
    - 在 global.css 中添加了饼图报告的样式类
    - 在 ReportController 中实现了饼图的初始化和交互效果
    - 更新了 ReportView.fxml 中的样式表引用

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/ReportController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/ReportController.java
@@ -3,12 +3,22 @@ package xyz.hashdog.rdm.ui.controller;
 import atlantafx.base.controls.Card;
 import atlantafx.base.controls.Tile;
 import atlantafx.base.theme.Styles;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -40,6 +50,67 @@ public class ReportController extends BaseKeyController<ServerTabController> imp
                 new PieChart.Data(FAKER.food().fruit(), rnd.nextInt(10, 30))
         );
         keys.setData(data);
+        keys.getStyleClass().add("donut-chart");
+        keys.setAnimated(true);
+        keys.setLabelsVisible(true);
+        for (PieChart.Data data1 : keys.getData()) {
+            setUpHoverEffectWithTooltip(data1);
+        }
         memory.setData(data2);
     }
+
+
+    private void setUpHoverEffectWithTooltip(PieChart.Data data) {
+        Tooltip tooltip = new Tooltip();
+        tooltip.setText(data.getName() + ": " + data.getPieValue());
+//        Tooltip.install(data.getNode(), tooltip);
+
+        data.getNode().setOnMouseEntered(event -> {
+            // 放大效果
+            data.getNode().setScaleX(1.1);
+            data.getNode().setScaleY(1.1);
+            // 使用节点自身的填充颜色创建阴影
+            Color shadowColor = getRegionBackgroundColor(data);
+            data.getNode().setEffect(new DropShadow(10, shadowColor));
+            tooltip.show(data.getNode(), event.getScreenX(), event.getScreenY() + 10);
+        });
+
+        data.getNode().setOnMouseExited(event -> {
+            // 恢复正常大小
+            data.getNode().setScaleX(1.0);
+            data.getNode().setScaleY(1.0);
+            data.getNode().setEffect(null);
+            tooltip.hide();
+        });
+        data.getNode().setOnMouseClicked(event -> {
+            System.out.println("Clicked: " + data.getName() + " - " + data.getPieValue());
+        });
+    }
+    private Color getRegionBackgroundColor(PieChart.Data data) {
+        // JavaFX PieChart 使用 CSS 样式来设置颜色
+        // 默认颜色序列
+        Color[] defaultColors = {
+                Color.web("#f3622d"), // 橙色
+                Color.web("#fba71b"), // 黄色
+                Color.web("#57b757"), // 绿色
+                Color.web("#41a9c9"), // 蓝色
+                Color.web("#4258c9"), // 深蓝色
+                Color.web("#9a42c8"), // 紫色
+                Color.web("#c84164"), // 粉色
+                Color.web("#888888")  // 灰色
+        };
+
+        // 获取数据在数据集中的索引
+        PieChart chart = data.getChart();
+        if (chart != null) {
+            int index = chart.getData().indexOf(data);
+            if (index >= 0) {
+                return defaultColors[index % defaultColors.length];
+            }
+        }
+
+        // 默认返回第一个颜色
+        return defaultColors[0];
+    }
+
 }
