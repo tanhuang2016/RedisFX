58492b7e4ee4b9f2d272f634afaa16ce701996b3
hod <1103617438@qq.com>
2025-08-09 21:14:40 +0800

    feat(ui): 添加 InfoTable 类并更新 ReportView 布局- 新增 InfoTable 类，用于表示信息表格数据 - 在 ReportView.fxml 中调整表格列的顺序和标题 - 优化用户界面显示，提高信息可读性

---
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/entity/InfoTable.java
@@ -0,0 +1,62 @@
+package xyz.hashdog.rdm.ui.entity;
+
+
+/**
+ * @author th
+ * @version 2.2.0
+ * @since 2025/8/9 21:35
+ */
+public class InfoTable implements ITable {
+
+    private String key;
+    private String type;
+    private String value;
+    private String size;
+
+    public InfoTable() {
+    }
+
+    // 获取所有属性名称
+    public  String[] getProperties() {
+        return new String[]{"#row", "type","key","value"};
+    }
+
+
+    public InfoTable(String key, String type, String value) {
+        this.key = key;
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
