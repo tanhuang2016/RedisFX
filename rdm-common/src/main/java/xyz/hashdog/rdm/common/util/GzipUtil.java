af9c471769aaabce5a95d11414757823e90025c2
1103617438@qq.com <1103617438@qq.com>
2025-08-18 12:15:14 +0800

    refactor(rdm-common): 优化 GzipUtil 类的压缩和解压缩逻辑
    - 添加日志记录功能，替换原有的 e.printStackTrace() 方法
    - 使用 try-with-resources 语句自动关闭资源，避免手动关闭导致的资源泄露
    - 优化空字符串判断方法，使用更简洁的 str.isEmpty() 方法
    -移除冗余的变量声明，简化代码结构

--- rdm-common/src/main/java/xyz/hashdog/rdm/common/util/GzipUtil.java
+++ rdm-common/src/main/java/xyz/hashdog/rdm/common/util/GzipUtil.java
@@ -1,5 +1,8 @@
 package xyz.hashdog.rdm.common.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -13,28 +16,19 @@ import java.util.zip.GZIPOutputStream;
  * @since 2023/8/9 12:37
  */
 public class GzipUtil {
+    protected static Logger log = LoggerFactory.getLogger(GzipUtil.class);
     /**
      * 使用gzip压缩字符串
      */
     public static byte[] compress(String str, Charset charset) {
-        if (str == null || str.length() == 0) {
+        if (str == null || str.isEmpty()) {
             return new byte[0];
         }
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        GZIPOutputStream gzip = null;
-        try {
-            gzip = new GZIPOutputStream(out);
+        try (GZIPOutputStream gzip = new GZIPOutputStream(out)) {
             gzip.write(str.getBytes(charset));
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (gzip != null) {
-                try {
-                    gzip.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+           log.error("compress Exception", e);
         }
         return out.toByteArray();
     }
@@ -47,13 +41,10 @@ public class GzipUtil {
             return "";
         }
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ByteArrayInputStream in = null;
-        GZIPInputStream zip = null;
         String decompressed = null;
-        try {
-            in = new ByteArrayInputStream(compressed);
-            zip = new GZIPInputStream(in);
+        try( ByteArrayOutputStream out = new ByteArrayOutputStream();
+             ByteArrayInputStream in= new ByteArrayInputStream(compressed);
+             GZIPInputStream zip = new GZIPInputStream(in); ) {
             byte[] buffer = new byte[1024];
             int offset = -1;
             while ((offset = zip.read(buffer)) != -1) {
@@ -61,21 +52,7 @@ public class GzipUtil {
             }
             decompressed = out.toString(charset.displayName());
         } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (zip != null) {
-                    zip.close();
-                }
-                if (in != null) {
-                    in.close();
-                }
-                if (out != null) {
-                    out.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            log.error("uncompress Exception", e);
         }
         return decompressed;
     }
