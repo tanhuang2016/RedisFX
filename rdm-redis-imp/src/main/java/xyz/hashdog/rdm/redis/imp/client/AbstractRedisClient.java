0b2eb9685226c57da87afbfe2c1ffe8b9e1579c2
hod <1103617438@qq.com>
2025-08-23 22:43:58 +0800

    build(rdm-ui): 更新应用版本和变更日志
    - 将应用版本从 2.2.2 升级到 2.3.0
    - 清空了应用更新日志，准备记录新的更新内容

--- rdm-redis-imp/src/main/java/xyz/hashdog/rdm/redis/imp/client/AbstractRedisClient.java
+++ rdm-redis-imp/src/main/java/xyz/hashdog/rdm/redis/imp/client/AbstractRedisClient.java
@@ -24,7 +24,7 @@ import java.util.function.Function;
 /**
  * 封装通用方法
  * @author th
- * @version 2.2.2
+ * @version 2.3.0
  * @since 2025/8/17 17:05
  */
 public abstract class AbstractRedisClient implements RedisClient {
