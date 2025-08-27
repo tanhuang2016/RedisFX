969e122b5eefa4d128b660143ae6baed7581efe2
1103617438@qq.com <1103617438@qq.com>
2025-08-27 13:58:45 +0800

    feat(LanguageManager): 添加获取系统支持的所有语言环境和翻译功能
    - 新增 getSupportedLocales 方法，用于获取系统支持的所有语言环境- 新增 language 方法，用于获取指定键的翻译
    - 为类和方法添加了注释，提高代码可读性

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/util/LanguageManager.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/util/LanguageManager.java
@@ -5,11 +5,19 @@ import xyz.hashdog.rdm.ui.Main;
 
 import java.util.*;
 
+/**
+ * @author th
+ */
 public class LanguageManager {
 
     public static final String BASE_NAME = "i18n.messages";
 
     public static Locale DEFAULT_LOCALE = Locale.getDefault();
+    /**
+     * 获取系统支持的所有语言环境
+     *
+     * @return 所有语言环境列表
+     */
     public static List<Locale> getSupportedLocales() {
         List<Locale> availableLocales = new ArrayList<>();
         // 获取系统支持的所有语言环境
@@ -32,6 +40,12 @@ public class LanguageManager {
         return availableLocales;
     }
 
+    /**
+     * 获取指定键的翻译
+     *
+     * @param key 键
+     * @return 翻译
+     */
     public static String language(String key) {
        return Main.RESOURCE_BUNDLE.getString(key);
     }
