cf6efa7ac37f89eae2142d74440253462540ccd9
1103617438@qq.com <1103617438@qq.com>
2025-09-12 16:54:08 +0800

    feat(rdm-ui): 添加 JSON 数据高亮显示功能
    - 在 TextJsonConvertHandler 中实现 JSON 数据的语法高亮显示
    - 在 ByteArrayController 中添加 JSON 数据的查看功能
    - 更新项目依赖，将 richtextfx 版本从 0

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/handler/ValueConvertHandler.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/handler/ValueConvertHandler.java
@@ -5,6 +5,10 @@ import javafx.scene.Parent;
 import java.nio.charset.Charset;
 
 /**
+ *  * 数据进来先责任链方式试探是否属于该处理类型
+ *  * 是该出里类型，提供处理方式，默认直接展示16进制数据
+ *  * 提供自定义上边栏按钮，按钮提供常用的模板导入，到处，弹框查看等
+ *  * 数据的输入，输出由处理器直接提供
  * @author th
  * @version 1.0.0
  * @since 2023/8/8 21:48
