d074a496ad10d8253b24e25eb66f39fbb4ba76de
hod <1103617438@qq.com>
2025-09-14 16:14:15 +0800

    feat(rdm-ui): 添加标准 Base64 编解码功能- 新增 Base64Converter 类，实现 DataConverter 接口- 添加 encode 和 decode 方法，分别用于 Base64 编码和解码 - 优化类注释，包含作者、版本和日期信息

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/handler/convert/Base64Converter.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/handler/convert/Base64Converter.java
@@ -1,4 +1,20 @@
 package xyz.hashdog.rdm.ui.handler.convert;
 
-public class Base64Converter {
+import java.util.Base64;
+/**
+ * 标砖base64编解码转换工具
+ * @author th
+ * @version 2.3.5
+ * @since 2025/9/13 22:48
+ */
+public class Base64Converter implements DataConverter{
+    @Override
+    public byte[] encode(byte[] data) {
+        return Base64.getEncoder().encode(data);
+    }
+
+    @Override
+    public byte[] decode(byte[] data) {
+        return Base64.getDecoder().decode(data);
+    }
 }
