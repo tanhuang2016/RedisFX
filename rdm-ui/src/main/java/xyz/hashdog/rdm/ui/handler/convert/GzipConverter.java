cddb1373fe8917cd190a4d599dff3e174154ea8d
hod <1103617438@qq.com>
2025-09-14 16:19:30 +0800

    feat(rdm-ui): 添加 Gzip 编解码转换功能
    - 实现了 GzipConverter 类，提供 gzip 压缩和解压缩功能
    - 该类实现了 DataConverter 接口，定义了 encode 和 decode 方法
    - 使用 ByteArrayInputStream 和 ByteArrayOutputStream 进行数据流处理
    - 异常情况下抛出 RuntimeException，确保错误处理的一致性

---
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/handler/convert/GzipConverter.java
@@ -0,0 +1,43 @@
+package xyz.hashdog.rdm.ui.handler.convert;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Gzip编解码转换工具
+ * @author th
+ * @version 2.3.5
+ * @since 2025/9/13 22:48
+ */
+public class GzipConverter implements DataConverter{
+    @Override
+    public byte[] encode(byte[] data) {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream)) {
+            gzipOutputStream.write(data);
+            gzipOutputStream.finish();
+            return byteArrayOutputStream.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to compress data using GZIP", e);
+        }
+    }
+
+    @Override
+    public byte[] decode(byte[] data) {
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+             GZIPInputStream gzipInputStream = new GZIPInputStream(byteArrayInputStream);
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = gzipInputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, len);
+            }
+            return outputStream.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to decompress data using GZIP", e);
+        }
+    }
+}
