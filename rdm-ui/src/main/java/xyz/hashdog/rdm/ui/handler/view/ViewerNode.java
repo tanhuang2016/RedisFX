66f8f9a1483060456888fd19e8622bbbedafaa5b
hod <1103617438@qq.com>
2025-09-15 22:56:54 +0800

    refactor(rdm-ui): 优化 ViewerNode 接口的 options 方法返回值
    - 将 options 方法的返回值从 null 修改为 Collections.emptyList()
    - 这样做可以避免调用方需要额外判断返回值是否为 null，提高代码的健壮性和可维护性

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/handler/view/ViewerNode.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/handler/view/ViewerNode.java
@@ -3,6 +3,7 @@ package xyz.hashdog.rdm.ui.handler.view;
 import javafx.scene.Node;
 import javafx.scene.control.MenuItem;
 
+import java.util.Collections;
 import java.util.List;
 
 public interface ViewerNode {
@@ -23,6 +24,6 @@ public interface ViewerNode {
      * 获取选项菜单
      */
     default List<MenuItem> options(){
-        return null;
+       return Collections.emptyList();
     }
 }
