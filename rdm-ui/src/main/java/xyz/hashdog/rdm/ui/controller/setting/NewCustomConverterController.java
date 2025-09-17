d472f965d3a3ea34c068064445cfff20e815227a
1103617438@qq.com <1103617438@qq.com>
2025-09-17 10:41:51 +0800

    refactor(rdm-ui): 重构自定义转换器界面并添加帮助图标
    - 重构了 NewCustomConverterController 中的 RadioButton 命名，提高了代码可读性- 在 RadioButton 上添加了帮助图标，增强了用户界面的直观性- 更新了 NewCustomConverterView.fxml，使用新的 RadioButton ID

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/NewCustomConverterController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/NewCustomConverterController.java
@@ -3,10 +3,7 @@ package xyz.hashdog.rdm.ui.controller.setting;
 import atlantafx.base.controls.ToggleSwitch;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.kordamp.ikonli.feather.Feather;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.kordamp.ikonli.material2.Material2AL;
@@ -24,9 +21,12 @@ public class NewCustomConverterController extends BaseWindowController<MainContr
     public Tab encodeTab;
     public TextField encodeCmd;
     public TextField encodeDir;
-    public RadioButton stdioInteractionButton;
-    public RadioButton fileInteractionButton;
+    public RadioButton encodeStdio;
+    public RadioButton encodeFile;
+    public RadioButton decodeStdio;
+    public RadioButton decodeFile;
     public Label help;
+
 
     @FXML
     public void initialize() {
@@ -43,6 +43,14 @@ public class NewCustomConverterController extends BaseWindowController<MainContr
         decodeTab.setGraphic(new FontIcon(Material2AL.LOG_OUT));
         encodeTab.setGraphic(new FontIcon(Material2AL.LOG_IN));
         help.setGraphic(new FontIcon(Material2AL.LIVE_HELP));
+        encodeStdio.setGraphic(new FontIcon(Material2AL.HELP_OUTLINE));
+        encodeStdio.setContentDisplay(ContentDisplay.RIGHT);
+        decodeStdio.setGraphic(new FontIcon(Material2AL.HELP_OUTLINE));
+        decodeStdio.setContentDisplay(ContentDisplay.RIGHT);
+        decodeFile.setGraphic(new FontIcon(Material2AL.HELP_OUTLINE));
+        decodeFile.setContentDisplay(ContentDisplay.RIGHT);
+        encodeFile.setGraphic(new FontIcon(Material2AL.HELP_OUTLINE));
+        encodeFile.setContentDisplay(ContentDisplay.RIGHT);
     }
 
 
