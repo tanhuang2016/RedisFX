a0bafe7f4402d3240631f327a06304c901e66505
1103617438@qq.com <1103617438@qq.com>
2025-09-17 10:34:54 +0800

    feat(ui): 新增自定义编解码器帮助图标和提示- 在 NewCustomConverterView.fxml 中添加了帮助图标 - 在 NewCustomConverterController.java 中增加了帮助图标的样式设置和鼠标悬停提示 - 优化了界面布局，调整了部分控件的属性

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/NewCustomConverterController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/NewCustomConverterController.java
@@ -2,6 +2,8 @@ package xyz.hashdog.rdm.ui.controller.setting;
 
 import atlantafx.base.controls.ToggleSwitch;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
@@ -24,16 +26,23 @@ public class NewCustomConverterController extends BaseWindowController<MainContr
     public TextField encodeDir;
     public RadioButton stdioInteractionButton;
     public RadioButton fileInteractionButton;
+    public Label help;
 
     @FXML
     public void initialize() {
 //        initLanguage();
         initIcon();
+        initStyles();
+    }
+
+    private void initStyles() {
+        help.setCursor(Cursor.HAND);
     }
 
     private void initIcon() {
         decodeTab.setGraphic(new FontIcon(Material2AL.LOG_OUT));
         encodeTab.setGraphic(new FontIcon(Material2AL.LOG_IN));
+        help.setGraphic(new FontIcon(Material2AL.LIVE_HELP));
     }
 
 
