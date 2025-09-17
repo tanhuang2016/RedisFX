dc06fd59a98ce18c2cee00d08589476192dd4e93
1103617438@qq.com <1103617438@qq.com>
2025-09-17 10:14:06 +0800

    feat(rdm-ui): 新增自定义编解码器功能
    - 添加了自定义编解码器的 UI 界面和控制器- 实现了编解码器的基本配置功能，包括解码器和编码器的命令、目录等设置
    - 增加了 stdio 交互和文件交互两种方式的选择
    - 使用了 FontIcon 图标库为界面添加图标- 优化了界面布局和控件属性，提高了用户体验

--- rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/NewCustomConverterController.java
+++ rdm-ui/src/main/java/xyz/hashdog/rdm/ui/controller/setting/NewCustomConverterController.java
@@ -1,7 +1,40 @@
 package xyz.hashdog.rdm.ui.controller.setting;
 
+import atlantafx.base.controls.ToggleSwitch;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import org.kordamp.ikonli.feather.Feather;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.material2.Material2AL;
+import org.kordamp.ikonli.material2.Material2MZ;
 import xyz.hashdog.rdm.ui.controller.MainController;
+import xyz.hashdog.rdm.ui.controller.ServerConnectionsController;
 import xyz.hashdog.rdm.ui.controller.base.BaseWindowController;
 
-public class NewCustomConverterController extends BaseWindowController<MainController> {
+public class NewCustomConverterController extends BaseWindowController<MainController>  {
+    @FXML
+    public ToggleSwitch enabled;
+    public Tab decodeTab;
+    public TextField decodeCmd;
+    public TextField decodeDir;
+    public Tab encodeTab;
+    public TextField encodeCmd;
+    public TextField encodeDir;
+    public RadioButton stdioInteractionButton;
+    public RadioButton fileInteractionButton;
+
+    @FXML
+    public void initialize() {
+//        initLanguage();
+        initIcon();
+    }
+
+    private void initIcon() {
+        decodeTab.setGraphic(new FontIcon(Material2AL.LOG_OUT));
+        encodeTab.setGraphic(new FontIcon(Material2AL.LOG_IN));
+    }
+
+
 }
