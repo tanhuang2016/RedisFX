0cc1ae19b02e87035625b3b8c257d8bf19efdf9f
1103617438@qq.com <1103617438@qq.com>
2025-10-16 14:02:55 +0800

    feat(redis):优化Jedis连接池配置
    - 将最大空闲连接数从2减少到1
    - 添加空闲连接检测周期和超时时间配置
    - 设置连接在池中最小空闲时间为30秒- 设置检测连接空闲时间间隔为10秒

--- rdm-redis-imp/src/main/java/redisfx/tanh/rdm/redis/imp/Constant.java
+++ rdm-redis-imp/src/main/java/redisfx/tanh/rdm/redis/imp/Constant.java
@@ -3,6 +3,8 @@ package redisfx.tanh.rdm.redis.imp;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.time.Duration;
+
 /**
  * @author th
  * @version 1.0.0
@@ -19,8 +21,13 @@ public class Constant {
         // 创建Jedis连接池配置对象
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(10);
-        poolConfig.setMaxIdle(2);
+        poolConfig.setMaxIdle(1);
         poolConfig.setTestOnBorrow(true);
+        // 设置空闲连接检测周期和超时时间
+        // 设置连接在池中最小空闲时间30秒
+        poolConfig.setSoftMinEvictableIdleDuration(Duration.ofSeconds(30));
+        // 检测连接空闲时间间隔10秒
+        poolConfig.setTimeBetweenEvictionRuns(Duration.ofSeconds(10));
         POOL_CONFIG=poolConfig;
     }
 }
